0e88118a87e91ff6bbf84943f0dd5f8271309fb2
Clemenchantee <cmonnier003@ensc.fr>
2023-03-27 18:52:59 +0200

    avancée sur les requetes

--- screens/InfosScreen.js
+++ screens/InfosScreen.js
@@ -1,68 +1,76 @@
-/*import React from "react";
+import React from "react";
 import { Text, View, ActivityIndicator, StyleSheet } from "react-native";
 import { useState } from 'react';
 import Input from "../component/Input";
 import 'firebase/firestore';
 import { déchetsCollection } from '../firebase';
-import { getDocs, query, where } from "firebase/firestore";
+import { db } from '../firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import Ionicons from "react-native-vector-icons/Ionicons";
-
 
 const InfosScreen = ({navigation}) => {
 
   const [loading, setLoading] = useState(false);
-  const [déchets, setDechets] = useState([]);
+  const [results, setResults] = useState([]);
 
   const searchDechets = async (name) => {
     setLoading(true);
 
     //requête bdd firestore
-    const q = query(déchetsCollection, where("nomDéchets", "==", name));
-    const querySnapshot = await getDocs(q);
-    const data = [];
+        //const q = query(déchetsCollection, where("nomDéchets", "==", name));
+        //const querySnapshot = await getDocs(q);
+        // Récupérer le document de déchets correspondant au nom recherché
+      const querySnapshot = await db.collection('déchets').where('nomDéchets', '==', name).get();
+      console.log('querySnapshot',querySnapshot)
+      const results = [];
 
-    querySnapshot.forEach((doc) => {
-        const docData = doc.data();
-        const { description, Poubelle } = docData;
-        data.push({ description, Poubelle });
+    // Parcourir les résultats
+    querySnapshot.forEach(async (doc) => {
+      const docdéchets = doc.data();
+      const { description, Poubelle } = docdéchets;
+      const poubelleRef = docdéchets.bac; // récupère la référence à la poubelle
+      const poubelleDoc = await poubelleRef.get(); // récupère le document de la poubelle
+      const poubelleData = poubelleDoc.data(); // récupère les données de la poubelle
+      const couleur = poubelleData.couleur; // extrait la couleur de la poubelle
+      results.push({ description, Poubelle: couleur });
     });
 
-    setDechets(data);
+   /*requête pour trouver les déchets 
+    querySnapshot.forEach((doc) => {
+        const docdéchets = doc.data();
+        const { description, Poubelle, nomDéchets } = docdéchets;
+        if (nomDéchets == name) {
+          const poubelle = docdéchets.bac.couleur; // récupère la couleur du bas associé
+          results.push({ description, Poubelle: `couleur: ${poubelle}` });
+        }
+    }); */
+
+    console.log('results', results);
+    console.log(`Nombre de résultats trouvés : ${results.length}`);
+
+    setResults(results);
     setLoading(false);
-};
+  };
 
   let mainComponent;
-  if (loading)
+  if (loading) {
     mainComponent = (
       <View style={styles.container}>
         <ActivityIndicator size="large" />
       </View>
     );
-
-  //voir cocktail search screen pour le else
-  const ResultList = ({data}) => {
-      <View>
-        {data.map((item, index) => (
-          <View key={index}>
-            <Text>{item.description}</Text>
-            <Text>{item.Poubelle}</Text>
-          </View>
-        ))}
-      </View>
   }
-  
-  {déchets.length > 0 && <ResultList data={déchets} />}
 
   return (
       <View style={[styles.container]}>
         <Text style={[styles.titre]}> Bienvenue à Bordeaux :))</Text>
         <Text style={styles.text}>ClearBin est là pour t'aider dans la gestion de tes déchets !</Text>
         <Text style={styles.question}>Quels déchets souhaites-tu trier ?</Text>
-        <Input style= {styles.inputContainer}
+        <Input style={styles.inputContainer}
           placeholder="Entrer un nom de déchet"
           onSubmit={searchDechets}
           renderLeftAccessory={() => (<Ionicons name="search-circle-outline" size={25} />)}/> 
-          {data.map((item, index) => (
+           {results.map((item, index) => (
           <View key={index}>
             <Text>{item.description}</Text>
             <Text>{item.Poubelle}</Text>
@@ -70,7 +78,7 @@ const InfosScreen = ({navigation}) => {
         ))}
       </View>
     );
-  };
+};
 
 export default InfosScreen;
 
@@ -82,89 +90,4 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: "row", alignItems: "center", backgroundColor: "#fff", borderWidth: 1, borderColor: "#ccc",
     borderRadius: 20, paddingLeft: 10, paddingRight: 10, height: 40, marginBottom: 10,},
-})*/
-
-import React from "react";
-import { Text, View, ActivityIndicator, StyleSheet } from "react-native";
-import { useState } from 'react';
-import Input from "../component/Input";
-import 'firebase/firestore';
-import { déchetsCollection } from '../firebase';
-import { getDocs, query, where } from "firebase/firestore";
-import Ionicons from "react-native-vector-icons/Ionicons";
-
-const InfosScreen = ({navigation}) => {
-
-  const [loading, setLoading] = useState(false);
-  const [déchets, setDechets] = useState([]);
-
-  const searchDechets = async (name) => {
-    setLoading(true);
-
-    //requête bdd firestore
-    const q = query(déchetsCollection, where("nomDéchets", "==", name));
-    const querySnapshot = await getDocs(q);
-    const data = [];
-
-    querySnapshot.forEach((doc) => {
-        const docData = doc.data();
-        const { description, Poubelle } = docData;
-        data.push({ description, Poubelle });
-    });
-
-    setDechets(data);
-    setLoading(false);
-  };
-
-  let mainComponent;
-  if (loading) {
-    mainComponent = (
-      <View style={styles.container}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  } else {
-    mainComponent = (
-      <>
-        {déchets.length > 0 && <ResultList data={déchets} />}
-        <Input style={styles.inputContainer}
-          placeholder="Entrer un nom de déchet"
-          onSubmitEditing={({ nativeEvent: { text } }) => searchDechets(text)}
-          renderLeftAccessory={() => (<Ionicons name="search-circle-outline" size={25} />)}
-        />
-      </>
-    );
-  }
-
-  return (
-    <View style={[styles.container]}>
-      <Text style={[styles.titre]}> Bienvenue à Bordeaux :))</Text>
-      <Text style={styles.text}>ClearBin est là pour t'aider dans la gestion de tes déchets !</Text>
-      <Text style={styles.question}>Quels déchets souhaites-tu trier ?</Text>
-      {mainComponent}
-    </View>
-  );
-};
-
-const ResultList = ({data}) => {
-  return (
-    <View>
-      {data.map((item, index) => (
-        <View key={index}>
-          <Text>{item.description}</Text>
-          <Text>{item.Poubelle}</Text>
-        </View>
-      ))}
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-  titre:{fontSize: 40, textAlign: 'center', marginBottom:20}, 
-  text: { fontSize: 18, paddingBottom: 10, textAlign: 'center', marginBottom:20},
-  question : { fontSize: 16, paddingBottom: 10 },
-  inputContainer: {
-    flexDirection: "row", alignItems: "center", backgroundColor: "#fff", borderWidth: 1, borderColor: "#ccc",
-    borderRadius: 20, paddingLeft: 10, paddingRight: 10}
-  })
+})
