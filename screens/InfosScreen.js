04aded34479be314e3b3f004ee3cc292b718708e
Clemenchantee <cmonnier003@ensc.fr>
2023-03-30 16:27:37 +0200

    ça marche pas

--- screens/InfosScreen.js
+++ screens/InfosScreen.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Text, View, ActivityIndicator, StyleSheet } from "react-native";
-import { useState } from 'react';
+import { Text, View, ActivityIndicator, StyleSheet , FlatList} from "react-native";
+import { useState, useEffect } from 'react';
 import Input from "../component/Input";
 import 'firebase/firestore';
 import { déchetsCollection } from '../firebase';
+import { PoubelleCollection } from "../firebase";
 import { db } from '../firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const InfosScreen = ({navigation}) => {
-
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState([]);
 
@@ -17,25 +17,14 @@ const InfosScreen = ({navigation}) => {
     setLoading(true);
 
     //requête bdd firestore
-        //const q = query(déchetsCollection, where("nomDéchets", "==", name));
-        //const querySnapshot = await getDocs(q);
-        // Récupérer le document de déchets correspondant au nom recherché
-      const querySnapshot = await db.collection('déchets').where('nomDéchets', '==', name).get();
-      console.log('querySnapshot',querySnapshot)
-      const results = [];
+    const q = query(déchetsCollection, where("nomDéchets", "==", name));
+    const querySnapshot = await getDocs(q);
+    console.log('querySnapshot',querySnapshot)
+    // Récupérer le document de déchets correspondant au nom recherché
+    /*const querySnapshot = await db.collection('déchets').where('nomDéchets', '==', name).get(); */
+    const results = [];
 
-    // Parcourir les résultats
-    querySnapshot.forEach(async (doc) => {
-      const docdéchets = doc.data();
-      const { description, Poubelle } = docdéchets;
-      const poubelleRef = docdéchets.bac; // récupère la référence à la poubelle
-      const poubelleDoc = await poubelleRef.get(); // récupère le document de la poubelle
-      const poubelleData = poubelleDoc.data(); // récupère les données de la poubelle
-      const couleur = poubelleData.couleur; // extrait la couleur de la poubelle
-      results.push({ description, Poubelle: couleur });
-    });
-
-   /*requête pour trouver les déchets 
+   //requête pour trouver les déchets 
     querySnapshot.forEach((doc) => {
         const docdéchets = doc.data();
         const { description, Poubelle, nomDéchets } = docdéchets;
@@ -43,7 +32,7 @@ const InfosScreen = ({navigation}) => {
           const poubelle = docdéchets.bac.couleur; // récupère la couleur du bas associé
           results.push({ description, Poubelle: `couleur: ${poubelle}` });
         }
-    }); */
+    });
 
     console.log('results', results);
     console.log(`Nombre de résultats trouvés : ${results.length}`);
@@ -61,10 +50,33 @@ const InfosScreen = ({navigation}) => {
     );
   }
 
+  //requête pour afficher les poubelles
+  const PoubelleScreen = () => {
+    const [poubelles, setPoubelles] = useState([]);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        const poubellesSnapshot = await PoubelleCollection.get();
+        const poubellesData = poubellesSnapshot.docs.map((doc) => doc.data());
+        setPoubelles(poubellesData);
+      };
+      fetchData();
+    }, []);
+  
+    const renderPoubelle = ({ item }) => (
+      <View>
+        <Text>{item.couleur}</Text>
+        <Text>{item.description}</Text>
+        <Text>{item.formeTri}</Text>
+        <Text>{item.recyclable}</Text>
+      </View>
+    );
+    }
+
   return (
       <View style={[styles.container]}>
         <Text style={[styles.titre]}> Bienvenue à Bordeaux :))</Text>
-        <Text style={styles.text}>ClearBin est là pour t'aider dans la gestion de tes déchets !</Text>
+        <Text style={styles.text}> ClearBin est là pour t'aider dans la gestion de tes déchets !</Text>
         <Text style={styles.question}>Quels déchets souhaites-tu trier ?</Text>
         <Input style={styles.inputContainer}
           placeholder="Entrer un nom de déchet"
@@ -75,19 +87,28 @@ const InfosScreen = ({navigation}) => {
             <Text>{item.description}</Text>
             <Text>{item.Poubelle}</Text>
           </View>
-        ))}
+           ))}
+        <Text style={styles.text}> Informations générales </Text>
+        <FlatList
+          data={poubelles}
+          renderItem={renderPoubelle}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     );
-};
+}
+
+
 
 export default InfosScreen;
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1},
   titre:{fontSize: 40, textAlign: 'center', marginBottom:20}, 
-  text: { fontSize: 18, paddingBottom: 10, textAlign: 'center', marginBottom:20},
-  question : { fontSize: 16, paddingBottom: 10 },
+  text: { fontSize: 18, paddingBottom: 10, textAlign: 'center', marginBottom:20, marginLeft: 10, marginRight : 10},
+  question : { fontSize: 16, paddingBottom: 10, marginLeft: 15},
   inputContainer: {
     flexDirection: "row", alignItems: "center", backgroundColor: "#fff", borderWidth: 1, borderColor: "#ccc",
     borderRadius: 20, paddingLeft: 10, paddingRight: 10, height: 40, marginBottom: 10,},
 })
+
